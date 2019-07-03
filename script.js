191d83266827cf81eacc3ca284050a46570514cc
MalihaKabir <malihakabir01@gmail.com>
2019-07-03 15:38:35 +0600

    deploy

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const client = contentful.createClient({
 	// This is the space ID. A space is like a project folder in Contentful terms
-	space       : 'YOUR_OWN_Space ID',
+	space       : 'qveid79p3kya',
 	// This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-	accessToken : 'YOUR_OWN_Content Delivery API - access token',
+	accessToken : 'vHQAQrRlSQC73Y1mejx2atATsGPAujM0BGujLsIhZNo',
 });
 
 // console.log(client);
